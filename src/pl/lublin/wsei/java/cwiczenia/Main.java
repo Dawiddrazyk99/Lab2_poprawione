d81e57c330fe4292488a568a69177998ecaef691
Dawid <dawiddrazyk99@o2.pl>
2021-07-06 10:56:37 +0200

    Lab_2_poprawione

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,15 @@
+package pl.lublin.wsei.java.cwiczenia;
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+//import mylib.Account;
+
+//import java.until.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Account acc = new Account();
+        acc.setName("Dawid Drążyk");
+        System.out.println(acc.getName());
+
+    }
+}
